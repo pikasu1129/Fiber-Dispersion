434d72243583f99962554f4f2fa7e1cb13338c5b
pikasu1129 <ueda730@gmail.com>
2022-05-20 01:09:41 +0900

    2022-05-20 01:08 25bitのランダム信号を生成　 時間軸　０→625ps とりあえず高羽さんのを真似た

--- 4km, 28GHz, 1Gbps.py
+++ 4km, 28GHz, 1Gbps.py
@@ -4,6 +4,10 @@ import math
 import matplotlib.pyplot as plt
 import random
 
+#8bitで1バイト　→　1GBpsは125MB/s
+#1Gbpsの1周期　→　1ns
+
+
 L = 0.1 #(km)
 bit = 25
 Lam = 1.55 * 10 ** -6 #(m)
@@ -26,13 +30,34 @@ polnumber = 1 + int(sumw) - initial
 electrodelength = pitch * polnumber
 elecctrodelengthmm = electrodelength * 10 ** 3
 
-#28GHzの信号を変調を生成
+#28GHzの信号
+#時間tを生成
+
+t = np.arange(0, bit * 25, 24)
 
 digital = []
+stepram = []
+for n in np.arange(0, bit * 25, 24):
+    rm = random.randint(0,1)
+    digital.append(rm)
 
-for n in range(0,bit):
-    digital[n] = random.randint(0,1)
+print(digital)
 
-print(digital[0])
+#x = np.arange(0, 24)
+y = digital
 
+plt.rcParams["font.size"] = 18
+plt.rcParams["font.family"] = "Bold"
 
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+ax.step(t, y, color = 'Red')
+
+ax.set_ylabel("Power")
+ax.set_xlabel("Time[ps]")
+#ax.set_xticklabels([0, 100, 200, 300, 400, 500, 600])
+plt.show()
+
+f = 28 * 10 ** 3 #(Hz)
+signal = math.cos(2 * math.pi * f)
